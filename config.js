af1ef8be7a07487b9cbd63dfb8cada18f335118f
alibaba0507 <fx2go4u@gmail.com>
2019-03-20 20:40:14 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.token = "6fd837423aa6681b9e163363b2c95969528dbf39"; // get this from http://tradingstation.fxcm.com/
+config.trading_api_host = 'api.fxcm.com';
+config.trading_api_port = 443;
+config.trading_api_proto = 'https'; // http or https
+
+module.exports = config;
