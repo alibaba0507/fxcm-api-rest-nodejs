1a02256c2ad2f0a5c753ff213e36e27bebe66837
alibaba0507 <fx2go4u@gmail.com>
2019-03-25 09:36:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.token = "6fd837423aa6681b9e163363b2c95969528dbf39"; // get this from http://tradingstation.fxcm.com/
+config.token = ""; // get this from http://tradingstation.fxcm.com/
 config.trading_api_host = 'api.fxcm.com';
 config.trading_api_port = 443;
 config.trading_api_proto = 'https'; // http or https
